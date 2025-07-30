3c397f7c6e0fa92c72ce2a40305c67649d324073
JoelDev13 <joel13r11@gmail.com>
2025-07-30 12:29:28 -0400

    chore: ejercicios de creacion y usode objetos

---
+++ Módulo1/Creación y usos de objetos/ConversoresUnidades/convertirTemperaturas/convertirTemperaturas/Program.cs
@@ -0,0 +1,20 @@
+﻿using convertirTemperaturas;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+       
+        TemperaturaFahrenheit tempF = new TemperaturaFahrenheit(100);
+        TemperaturaCelsius tempC = tempF.ConvertirACelsius();
+
+        Console.WriteLine($"Fahrenheit: {tempF}");
+        Console.WriteLine($"Convertido a Celsius: {tempC}");
+
+        TemperaturaCelsius otraTempC = new TemperaturaCelsius(0);
+        TemperaturaFahrenheit otraTempF = otraTempC.ConvertirAFahrenheit();
+
+        Console.WriteLine($"Celsius: {otraTempC}");
+        Console.WriteLine($"Convertido a Fahrenheit: {otraTempF}");
+    }
+}
---
+++ Módulo1/Creación y usos de objetos/ConversoresUnidades/convertirTemperaturas/convertirTemperaturas/TemperaturaCelsius.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertirTemperaturas
+{
+    public class TemperaturaCelsius
+    {
+        double GradoCelsius {  get; set; }
+
+        public TemperaturaCelsius(double gradoCelsius)
+        {
+            GradoCelsius = gradoCelsius;
+        }
+        public TemperaturaFahrenheit ConvertirAFahrenheit()
+        {
+            double fahrenheit = (GradoCelsius * 9 / 5) + 32;
+            return new TemperaturaFahrenheit(fahrenheit);
+        }
+        public override string ToString()
+        {
+            return $"{GradoCelsius} °C";
+        }
+    }
+}
